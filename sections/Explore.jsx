3d202ea1d98d2a35a08e85900ba5aea8a3ba5201
smirnovsky <happy280190@gmail.com>
2023-05-25 14:08:22 +0300

    start to implement Explore component

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,25 @@
 'use client';
 
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+
+import styles from '../styles'
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '../constants/index';
+
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id='explore'>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title='| The world' textStyles='text-center' />
+      <TitleText title={<>Choose the world you want <br className="md:block hidden" /> to explore</>} textStyles='text-center' />
+    </motion.div>
   </section>
 );
 
