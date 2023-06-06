0e1cc2417c445160e253e650fe0ef8fb101738c0
smirnovsky <happy280190@gmail.com>
2023-06-06 18:14:45 +0300

    add transitions for button in Explore card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -10,7 +10,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${
       active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
-    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer `}
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.4s] ease-out-flex cursor-pointer `}
     onClick={() => handleClick(id)}
   >
     <img
@@ -19,12 +19,12 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[90deg] lg:origin-[0,0] ">
+      <h3  className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[90deg] lg:origin-[0,0] ">
         {title}
       </h3>
     ) : (
-      <div className=" absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0)] rounded-b-[24px]">
-        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px] relative `}>
+      <div className=" absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
+        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px] relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 `}>
           <img
             src='/headset.svg'
             alt='headset'
